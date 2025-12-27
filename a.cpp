e4d7e6ddf845f0e6f987c2055b012dc2dd50ac57
797abhijeet <abhijeetsaxena615@gmail.com>
2025-12-27 23:21:08 +0530

    folder updated

--- a.cpp
+++ a.cpp
@@ -1,48 +1,67 @@
 #include <bits/stdc++.h>
 using namespace std;
+using ll = long long;
 
-void solve() {
-    int n, k, x;
-    cin >> n >> k >> x;
+void solve()
+{
 
-    if (x != 1) {
-        cout << "YES\n";
-        cout << n << "\n";
-        for (int i = 0; i < n; i++) cout << 1 << " ";
-        cout << "\n";
-        return;
+    // table 
+    // 0 0 0 
+    // 1 0 1 
+    // 0 1 1 
+    // 1 1 0
+
+
+
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i];
     }
 
-   
-    if (k == 1) {
-        cout << "NO\n";
-        return;
+    if (n % 2 == 0)
+    {
+        int x = 0;
+        for (auto it : v)
+        {
+            x ^= it;
+        }
+        if (x == 0)
+        {
+            cout << 1 << endl;
+        }
+        else
+        {
+            cout << -1 << endl;
+        }
     }
-
-    if (n % 2 == 0) {
-        cout << "YES\n";
-        cout << n / 2 << "\n";
-        for (int i = 0; i < n / 2; i++) cout << 2 << " ";
-        cout << "\n";
-        return;
-    }
-
-    // n is odd
-    if (k >= 3) {
-        cout << "YES\n";
-        cout << (n - 3) / 2 + 1 << "\n";
-        for (int i = 0; i < (n - 3) / 2; i++) cout << 2 << " ";
-        cout << 3 << "\n";
-    } else {
-        cout << "NO\n";
+    else
+    {
+        int x = 0;
+        for (auto it : v)
+        {
+            D
+                x ^= it;
+        }
+        cout << x << endl;
     }
 }
 
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    freopen("error.txt", "w", stderr);
+#endif
 
     int t;
     cin >> t;
-    while (t--) solve();
+
+    while (t--)
+        solve();
+
+    return 0;
 }
